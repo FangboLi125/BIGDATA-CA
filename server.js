7c14b2af3f3d05e2560ade48dba574652dbaae20
FangboLi125 <fangboli1989@gmail.com>
2020-06-05 14:55:02 +0000

    blog

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+
+const mongoose =require('mongoose')
+
+const bodyParser =  require('body-parser')
+
+
+//to connect to database
+mongoose.connect("mongodb+srv://test:ccttestuser@cluster0-sbohj.mongodb.net/blog?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true})
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(express.static('views'));
+
+app.set("view engine","ejs")
+
+var postSchema = new mongoose.Schema({body:String})
+
+var post = mongoose.model('post',postSchema)
+
+app.post('/addpost',(req,res) => {
+      var postData = new post(req.body)
+      
+      postData.save().then(result => {
+          console.log(result)
+            res.redirect('/')
+            }).catch(err => {
+                res.status(400).send("Unable to save data")
+             })
+})
+
+app.get('/',(req, res) => {
+  post.find({}, (err,posts) => {
+      res.render('index',{posts:posts})
+             })
+
+})
+
+app.listen(3000,() =>{
+console.log("Server is listening on Port 3000")
+
+})
